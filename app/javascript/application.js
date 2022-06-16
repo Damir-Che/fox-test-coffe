c69a6532ace6de6af0722385f215930c6a286169
Damir <chuchehov@gmail.com>
2022-06-16 21:34:38 +0300

    tg

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -5,3 +5,14 @@ import "controllers"
 //= require jquery3
 //= require popper
 //= require bootstrap-sprockets
+
+
+let tg = window.Telegram.WebApp;
+
+console.log(tg)
+
+tg.MainButton.text = "Order"; //изменяем текст кнопки
+// tg.MainButton.setText("Changed Text1"); //изменяем текст кнопки иначе
+tg.MainButton.textColor = "#ffffff"; //изменяем цвет текста кнопки
+tg.MainButton.color = "#F55353"; //изменяем цвет бэкграунда кнопки
+// tg.MainButton.setParams({"color": "#143F6B"});
